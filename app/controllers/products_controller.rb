a2b46f7af52846c8f29c722cb13190c05a421699
girik <girikaustuv@gmail.com>
2021-12-06 20:25:26 +0545

    CheckOut

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,8 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
+  
   end
 
   # GET /products/1 or /products/1.json
@@ -40,6 +41,9 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to @product, notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+        @products = Product.all.order(:title)
+ ActionCable.server.broadcast 'products',
+ html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
@@ -67,3 +71,14 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
 end
+
+def who_bought
+  @product = Product.find(params[:id])
+  @latest_order = @product.orders.order(:updated_at).last
+  if stale?(@latest_order)
+  respond_to do |format|
+  format.atom
+  end
+  end
+  end
+  
